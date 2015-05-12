e8293f9222d7937e15930084438736b20ca35bec
David Begin <davidmichaelbe@gmail.com>
2015-05-12 10:31:34 -0700

    expanding winners example

--- built_in_conversion_protocols.rb
+++ built_in_conversion_protocols.rb
@@ -1,3 +1,45 @@
+class WinnerCircle
+  def initialize(winners)
+    @winners = winners
+  end
+
+  def annoucement
+    [first, second, third].each do |place|
+      puts "#{winners[place]} wins #{place.name}"
+      puts "Winning: #{place.prize}\n"
+    end
+  end
+
+  private
+
+  attr_reader :winners
+
+  def first
+    @first ||= Place.new(
+      0, "first", "all the money in the world"
+    )
+  end
+
+  def second
+    @second ||= Place.new(
+      1, "second", "unlimited candy supply"
+    )
+  end
+
+  def third
+    @third ||= Place.new(
+      2, "third", "gift card to Powells"
+    )
+  end
+
+  Place = Struct.new(:index, :name, :prize) do
+    def to_int
+      index
+    end
+  end
+
+end
+
 winners = [
   "Kent Beck",
   "DHH",
@@ -6,17 +48,16 @@ winners = [
   "Uncle Bob"
 ]
 
-Place = Struct.new(:index, :name, :prize) do
-  def to_int
-    index
-  end
-end
+WinnerCircle.new(winners).annoucement
 
-first = Place.new(0, "first", "all the money in the world")
-second = Place.new(1, "second", "unlimited candy supply")
-third = Place.new(2, "third", "gift card to Powells")
+__END__
 
-[first, second, third].each do |place|
-  puts "#{winners[place]} wins #{place.name}"
-  puts "Winning: #{place.prize}\n"
-end
+Often times when coding, I am looking to create code
+that is unsuprising.
+  And I feel like defining and relying on implicit conversions,
+  could be confusing the someone new to a code base.
+
+  At the same time, it makes sense, to see if the objects we
+have can play the roles we need, and if they only need a simple
+implicit conversion definined, then thats a big win.
+
