4afa08f671c646bff22c663297493eb00c73cbd2
David Begin <davidmichaelbe@gmail.com>
2015-05-20 22:05:15 -0700

    exploring module_function advantages and uses

---
+++ module_function.rb
@@ -0,0 +1,25 @@
+# What is the real advantage of module_function
+#
+# why use if over self << class
+#
+# or extending a module
+
+module Toolbox1
+  def self.stuff1
+    "stuff1"
+  end
+end
+
+module Toolbox2
+  module_function
+
+  def stuff2
+    "stuff2"
+  end
+end
+
+module Toolbox3
+  def stuff3
+    "stuff3"
+  end
+end
